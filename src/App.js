e455818db1fe61c1aaff04c612970e363c139292
Arthur Lacombe <arthur.lacombe@adp.com>
2019-11-21 09:38:09 +0100

    v06

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Grid, GridColumn } from 'semantic-ui-react';
 import Messages from './components/Messages/Messages';
 import "./App.css";
-import ColorPanel from './components/ColorPanel/ColorPanel';
 import MetaPanel from './components/MetaPanel/MetaPanel';
 import SidePanel from './components/SidePanel/SidePanel';
 import { connect } from 'react-redux';
@@ -11,7 +10,7 @@ import { connect } from 'react-redux';
 const App = ({ currentUser }) =>
 (
   <Grid columns="equal" className="app" style={{ background: '#eee' }}>
-    <ColorPanel />
+    
     <SidePanel currentUser={currentUser} />
     <GridColumn style={{ marginLeft: 320 }}>
       <Messages />
